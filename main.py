bf89ccddc781b7e4d3f27935479769d97f56a41b
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-05-21 12:42:52 -0500

    Refactor main execution flow to improve dataset handling and add directory validation

--- main.py
+++ main.py
@@ -3,17 +3,47 @@ import smeargle
 import porygon
 import miscellaneous
 
-USE_LOCAL_STORAGE = False
-INPUT_DIR = OUTPUT_DIR = TRAINING_DIR = ''
-
 if __name__ == "__main__":
-    USE_LOCAL_STORAGE = miscellaneous.make_a_choice('\nUse local storage? Y/n: ', 'n')[1]
+    # -------------------------------
+    # Configuration
+    # -------------------------------
+    input_shape = (128, 128)
+    num_classes = 2
+    author = 'benjaminadedowole'
+    dataset_name = 'wartortle-evolution-error'
+    CARD_DIM = (480, 680)
+    ROI_BOX = (40, 45, 60, 60)  # (x, y, width, height)
+
+    # -------------------------------
+    # Preamble
+    # -------------------------------
+    _, USE_LOCAL_STORAGE = miscellaneous.make_a_choice('\nUse local storage? Y/n: ', 'n')
+    INPUT_DIR = OUTPUT_DIR = ''
     if USE_LOCAL_STORAGE:
-        INPUT_DIR, _ = miscellaneous.make_a_choice('Provide the directory path for storing downloaded images: ', 'images')
+        INPUT_DIR, _ = miscellaneous.make_a_choice('Provide the directory path for storing eBay images: ', 'images')
         OUTPUT_DIR, _ = miscellaneous.make_a_choice('Provide the directory path for storing debugging images: ', 'adjusted_images')
-        TRAINING_DIR, _ = miscellaneous.make_a_choice('Provide the directory path for storing image dataset: ', 'training_data')
+    TRAINING_DIR, _ = miscellaneous.make_a_choice('Provide the directory path for storing model dataset: ', 'dataset')
+    _, USE_RGB = miscellaneous.make_a_choice('Use RGB? Y/n: ', 'y')
     
     miscellaneous.clear_terminal()
+
+    directoryCheck = miscellaneous.directory_check(TRAINING_DIR)
+    attempts = 3
+    while not directoryCheck[0]:
+        if attempts<0:
+            exit()
+        TRAINING_DIR = porygon.get_dataset(TRAINING_DIR, author, dataset_name)
+        attempts-=1
+        directoryCheck = miscellaneous.directory_check(TRAINING_DIR)
+    images, labels, filenames = porygon.load_dataset_from_directory(TRAINING_DIR, input_shape)
+    porygon.display_sample(images, labels, filenames)
+    new_images, new_labels = porygon.convert_and_reshape(images, labels)
+    training_images, testing_images, training_labels, testing_labels = porygon.split_dataset(new_images, new_labels)
+    AI = porygon.build_model(num_classes, USE_RGB)
+    porygon.train_model(AI, training_images, training_labels)
+    porygon.evaluate_model(AI, testing_images, testing_labels)
+    porygon.predict_and_visualize(AI, testing_images, testing_labels)
+
 
     CLIENT_ID, CLIENT_SECRET = miscellaneous.credentials()
 
@@ -39,7 +69,7 @@ if __name__ == "__main__":
     miscellaneous.pause(10)
 
     for item in items:
-        print(f"\n\033[34m[INFO] Processing {item['title']}...\033[0m")
+        print(f"\033[34m[INFO] Processing {item['title']}...\033[0m")
         image = None; path = ''
         if USE_LOCAL_STORAGE:
             image, path = smeargle.load_file_from_directory(item['title'], INPUT_DIR, OUTPUT_DIR)
@@ -49,21 +79,11 @@ if __name__ == "__main__":
 
         approx = smeargle.detect_contours(image_edges)
         if len(approx) != 4:
-            print(f"\033[33m[WARNING] Skipping '{item['title']}' — could not detect card corners.\033[0m")
+            print(f"\033[33m[WARNING] Skipping '{item['title']}' — could not detect card corners.\033[0m\n")
             continue
 
-        aligned = smeargle.draw_contours(image, approx, path)
-        roi = smeargle.roi_extraction(aligned, path)
-        print(f'\033[32m[SUCCESS] Finished Processing {item['title']}\033[0m')
+        aligned = smeargle.draw_contours(image, approx, path, CARD_DIM)
+        roi = smeargle.roi_extraction(aligned, path, ROI_BOX)
+        print(f'\033[32m[SUCCESS] Finished Processing {item['title']}\033[0m\n')
 
     miscellaneous.pause(10)
-
-    images, labels, filenames = porygon.load_dataset_from_directory(TRAINING_DIR)
-    porygon.display_sample(images, labels, filenames)
-    new_images, new_labels = porygon.convert_and_reshape(images, labels)
-    training_images, testing_images, training_labels, testing_labels = porygon.split_dataset(new_images, new_labels)
-    AI = porygon.build_model()
-    porygon.train_model(AI, training_images, training_labels)
-    porygon.evaluate_model(AI, testing_images, testing_labels)
-    porygon.predict_and_visualize(AI, testing_images, testing_labels)
-
