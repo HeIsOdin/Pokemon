6a2bf2c1aac576a2d84a2f0907563a5f6f952b99
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-05-22 03:30:26 -0500

    Refactor HTML form in PokéPrint Alert for improved structure and add marketplace selection; update script.js to load defect options dynamically

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,16 @@
-alert("Welcome to PyPikachu");
+function load_options_from_JSON() {
+    fetch('../config.json').then(response => response.json()).then(data => {
+        const defect = document.getElementById("defect");
+		//const marketplace = document.getElementById("marketplace");
+        for (const key in data) {
+			const option = document.createElement("option");
+			option.value = key;
+			option.textContent = data[key]['title'];
+			defect.appendChild(option);
+		}
+	});
+};
+
+function body() {
+	load_options_from_JSON();
+};
