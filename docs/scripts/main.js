7414eaa1cd5c0c6331b5b334ae4fad7af49bb9d3
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-06-04 22:00:02 -0500

    Refactor settings form handling and update table structure in index.html

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (getCookie('url') && getCookie('state') === 'active') body();
+    else callHamster();
+});
+
 (function($) {
 
 	var	$window = $(window),
@@ -393,3 +398,84 @@
 					});
 
 })(jQuery);
+
+function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (let cookie of cookies) {
+        let [key, val] = cookie.split('=');
+        if (key === name) {
+            if (val !== "") return decodeURIComponent(val);
+        }
+    }
+  return null;
+}
+
+function callHamster(url='hamster.html') {
+    const currentUrl = window.location.href;
+
+	const expires = new Date(Date.now() + 6e5).toUTCString();
+    document.cookie = `caller=${encodeURIComponent(currentUrl)}; expires=${expires}; path=/; SameSite=Lax; Secure`;
+
+    window.location.replace('/Pokemon/pages/' + url);
+}
+
+let info, data;
+async function body() {
+	const form = document.querySelector('form')
+	if (!form) return
+	
+	form.action = getCookie('url')+'/settings';
+	const url = form.action.replace('settings', 'user-info');
+	let data_and_info;
+    try {
+	    const response = await fetch(url, {
+		    headers: {
+                "ngrok-skip-browser-warning": "true",
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+	    });
+        data_and_info = await response.json();
+		info = data_and_info['info'];
+		data =data_and_info['data'];
+
+        if ('redirect' in defectConfig) callHamster(defectConfig['redirect']); 
+    } catch {
+		callHamster();
+    } finally {
+		const table = document.querySelector("table");
+  		const thead = table.querySelector("thead");
+  		const tbody = table.querySelector("tbody");
+		
+		thead.innerHTML = ""; tbody.innerHTML = "";
+		
+		if (info.length > 0) {
+			const keys = Object.keys(info[0]);
+			const headerRow = document.createElement("tr");
+    	
+			keys.forEach(key => {
+				const th = document.createElement("th");
+				th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+				headerRow.appendChild(th);
+			});
+			thead.appendChild(headerRow);
+			
+			info.forEach(item => {
+				const row = document.createElement("tr");
+				
+				keys.forEach(key => {
+					const td = document.createElement("td");
+					td.textContent = item[key];
+					row.appendChild(td);
+				});
+				tbody.appendChild(row);
+			});
+		}
+
+  		const inputs = form.querySelectorAll("input[type='text']") + form.querySelectorAll("input[type='text']");
+		inputs.forEach(input => {
+			const key = input.id;
+			if (data.hasOwnProperty(key)) input.value = data[key];
+		});
+	}
+}
