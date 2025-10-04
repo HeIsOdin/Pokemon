f12a83379a97fff7adcf2580429404a4700e1c8b
lavic001 <lawalvictor2007@gmail.com>
2025-10-04 13:59:32 +0100

    Update main.js

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -399,32 +399,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 })(jQuery);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const themeRadios = document.querySelectorAll('input[name="theme"]');
-    const body = document.body;
-
-    // Load saved theme from localStorage
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        body.classList.remove('is-preload', 'light-theme');
-        body.classList.add(`${savedTheme}-theme`);
-        document.querySelector(`input[value="${savedTheme}"]`).checked = true;
-    }
-
-    themeRadios.forEach(radio => {
-        radio.addEventListener('change', (event) => {
-            const selectedTheme = event.target.value;
-
-            // Update body class
-            body.classList.remove('is-preload', 'light-theme');
-            body.classList.add(`${selectedTheme}-theme`);
-
-            // Save theme preference to localStorage
-            localStorage.setItem('theme', selectedTheme);
-        });
-    });
-});
-
 function getCookie(name) {
     const cookies = document.cookie.split('; ');
     for (let cookie of cookies) {
@@ -562,6 +536,7 @@ async function body() {
   		.then(data => window.location.replace('/Pokemon/pages/' + data.redirect));
 	});
 }
+
 
 
 
