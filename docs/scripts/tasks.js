00c4d6da649ca266752fc0b89d34ad8943a14115
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-10-05 14:00:02 -0500

    Fix script path case sensitivity and implement user info fetching in tasks.js

--- docs/scripts/tasks.js
+++ docs/scripts/tasks.js
@@ -1,1 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (getCookie('url') && getCookie('state') === 'active') body();
+    else callHamster();
+});
 
+function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (let cookie of cookies) {
+        let [key, val] = cookie.split('=');
+        if (key === name) {
+            if (val !== "") return decodeURIComponent(val);
+        }
+    }
+  return null;
+}
+
+let info;
+async function body() {
+    let url = getCookie('url') + '/user-info';
+    try {
+	    const response = await fetch(url, {
+		    headers: {
+                "ngrok-skip-browser-warning": "true",
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+	    });
+        info = await response.json()['info'];
+		if ('redirect' in data_and_info) callHamster(data_and_info['redirect']);
+    } catch {
+		callHamster();
+    } finally {
+		const table = document.querySelector("table");
+  		const thead = table.querySelector("thead");
+  		const tbody = table.querySelector("tbody");
+		
+		thead.innerHTML = ""; tbody.innerHTML = "";
+		
+		if (info.length > 0) {
+			const keys = Object.keys(info[0]);
+			const headerRow = document.createElement("tr");
+    	
+			keys.forEach(key => {
+				const th = document.createElement("th");
+				th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+				headerRow.appendChild(th);
+			});
+			thead.appendChild(headerRow);
+			
+			info.slice(0,3).forEach(item => {
+				const row = document.createElement("tr");
+				
+				keys.forEach(key => {
+					const td = document.createElement("td");
+					td.textContent = item[key];
+					row.appendChild(td);
+				});
+				tbody.appendChild(row);
+			});
+		}
+    }
+}
