5646065968a05463d2b7db5e2ce006d3fc66363a
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-11-09 11:52:24 -0600

    Add credentials option to headerOptions in hamster.js

--- docs/scripts/hamster.js
+++ docs/scripts/hamster.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://7fe038f5dcfd.ngrok-free.app/pypikachu'
 const headerOptions = {
   'ngrok-skip-browser-warning': 'true',
+    credentials: 'include'
 };
 
 async function logoAnimation() {
