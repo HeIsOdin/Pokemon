4c4453fc3106640eac63d4293eee8456edecbdb8
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-11-14 05:40:31 -0600

    Update BASE_URL in env.js to new ngrok address for correct API endpoint

--- docs/scripts/env.js
+++ docs/scripts/env.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://93d55831773e.ngrok-free.app/pypikachu'
+const BASE_URL = 'https://reeligible-extravagantly-jong.ngrok-free.dev/pypikachu'
 const fetchInit = {
   headers: {'ngrok-skip-browser-warning': 'true'},
   credentials: 'include',
